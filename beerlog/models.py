768b84a74d07bdc40819a1cb997981a25867f416
Anderson <48776969+andersoncleyson@users.noreply.github.com>
2022-04-26 18:40:29 +0000

    SQLModel

--- beerlog/models.py
+++ beerlog/models.py
@@ -1,7 +1,15 @@
-from dataclasses import dataclass
+from typing import Optional
+from sqlmodel import SQLModel, Field
+from sqlmodel import select
 
-class Beer:
+class Beer(SQLModel, table=True):
+  id: Optional[int] = Field(primary_key=True, default=None, index=True)
   name: str
   style: str
   flavor: int
+  image: int
+  cost: int
+
+#Objeto
+brewdog = Beer(name="Sewdog", style="NEIPA", flavor=6, image=8, cost=10)
   
